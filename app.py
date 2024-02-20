4cbe6d7bcf04526ee7045a9d503cc5c2f4125b2a
Nokokiii <kubanokoki@gmail.com>
2024-02-20 18:51:38 +0100

    app structure

--- app.py
+++ app.py
@@ -1,52 +1,30 @@
-from flask import Flask, jsonify, Response, request
-
-CREATED = 201
-BAD_REQUEST = 400
-SUCCESS = 200
+from flask import Flask
+from .business_logic import Logic
 
 app = Flask(__name__)
+logic = Logic()
 
 
 @app.route("/users", methods=["GET"])
 def get_users():
-    """
-    Zwraca listę zarejestrowanych użytkowników
-    """
-    # return get_all_users()
-    return jsonify({"users": []}), SUCCESS
+    return logic.users_list_controller()
 
 
-@app.route("/users/<id>", methods=["GET"])
-def get_user(id):
-    """
-    Zwraca dane użytkownika
-    """
-    # return get_user(id)
-    return jsonify({}), SUCCESS
+@app.route("/users/<user_id>", methods=["GET"])
+def get_user(user_id):
+    return logic.user_controller(user_id)
 
 
 @app.route("/users", methods=["POST"])
 def create_user():
-    """
-    Tworzy nowego użytkownika
-    """
-    # return create_user()
-    return Response(status=CREATED)
+    return logic.create_user_controller()
 
 
-@app.route("/users/<id>", methods=["PATCH"])
-def update_user(id):
-    """
-    Aktualizuje dane użytkownika
-    """
-    # status_code = update_user(id)
-    return Response(status=SUCCESS)
+@app.route("/users/<user_id>", methods=["PATCH"])
+def update_user(user_id):
+    return logic.update_user_controller(user_id)
 
 
-@app.route("/users/<id>", methods=["DELETE"])
-def delete_user(id):
-    """
-    Usuwa użytkownika
-    """
-    # status_code = delete_user(id)
-    return Response(status=SUCCESS)
+@app.route("/users/<user_id>", methods=["DELETE"])
+def delete_user(user_id):
+    return logic.delete_user_controller(user_id)
