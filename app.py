dd40b8b3f63a3e550f482968210280a7306f8239
Nokokiii <kubanokoki@gmail.com>
2024-02-20 20:45:55 +0100

    added data json

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
 from .business_logic import Logic
 
 app = Flask(__name__)
 logic = Logic()
+
+
+@app.errorhandler(404)
+def page_not_found():
+    return jsonify({'message': 'Endpoint not found'}), 404
 
 
 @app.route("/users", methods=["GET"])
@@ -10,21 +16,23 @@ def get_users():
     return logic.users_list_controller()
 
 
-@app.route("/users/<user_id>", methods=["GET"])
+@app.route("/users/<str:user_id>", methods=["GET"])
 def get_user(user_id):
     return logic.user_controller(user_id)
 
 
 @app.route("/users", methods=["POST"])
 def create_user():
-    return logic.create_user_controller()
+    data = request.get_json()
+    return logic.create_user_controller(data)
 
 
-@app.route("/users/<user_id>", methods=["PATCH"])
-def update_user(user_id):
-    return logic.update_user_controller(user_id)
+@app.route("/users/<str:user_id>", methods=["PATCH"])
+def update_user(user_id: str):
+    data = request.get_json()
+    return logic.update_user_controller(user_id, data)
 
 
-@app.route("/users/<user_id>", methods=["DELETE"])
-def delete_user(user_id):
+@app.route("/users/<str:user_id>", methods=["DELETE"])
+def delete_user(user_id: str):
     return logic.delete_user_controller(user_id)
