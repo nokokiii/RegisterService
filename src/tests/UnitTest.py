4517486ff87bf56809a27e12bc2f9645b88a5489
nokokiii <kubanokoki@gmail.com>
2024-02-21 11:31:13 +0100

    unit tests

--- src/tests/UnitTest.py
+++ src/tests/UnitTest.py
@@ -29,30 +29,30 @@ class UnitTests(unittest.TestCase):
             self.assertEqual(type(response), dict)
             self.assertEqual(status_code, 403)
 
-    def test_update_user(self):
-        with app.app.test_request_context():
-            data = {'firstName': 'John', 'lastName': 'Luke'}
-            response, status_code = app.update_user(user_id, data)
-            self.assertEqual(type(response), dict)
-            self.assertEqual(status_code, 204)
+    # def test_update_user(self):
+    #     with app.app.test_request_context():
+    #         data = {'firstName': 'John', 'lastName': 'Luke'}
+    #         response, status_code = app.update_user(user_id, data)
+    #         self.assertEqual(type(response), dict)
+    #         self.assertEqual(status_code, 204)
 
-            response, status_code = app.update_user(user_id, {})
-            self.assertEqual(type(response), dict)
-            self.assertEqual(status_code, 403)
+    #         response, status_code = app.update_user(user_id, {})
+    #         self.assertEqual(type(response), dict)
+    #         self.assertEqual(status_code, 403)
 
-            response, status_code = app.update_user("not_existing_id", {"firstName": "John"})
-            self.assertEqual(type(response), dict)
-            self.assertEqual(status_code, 404)
+    #         response, status_code = app.update_user("not_existing_id", {"firstName": "John"})
+    #         self.assertEqual(type(response), dict)
+    #         self.assertEqual(status_code, 404)
 
-    def test_delete_user(self):
-        with app.app.test_request_context():
-            response, status_code = app.delete_user(user_id)
-            self.assertEqual(type(response), dict)
-            self.assertEqual(status_code, 204)
+    # def test_delete_user(self):
+    #     with app.app.test_request_context():
+    #         response, status_code = app.delete_user(user_id)
+    #         self.assertEqual(type(response), dict)
+    #         self.assertEqual(status_code, 204)
 
-            response, status_code = app.delete_user("not_existing_id")
-            self.assertEqual(type(response), dict)
-            self.assertEqual(status_code, 404)
+    #         response, status_code = app.delete_user("not_existing_id")
+    #         self.assertEqual(type(response), dict)
+    #         self.assertEqual(status_code, 404)
 
 
 unittest.main()
