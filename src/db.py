8f167666f468db74dc4529a6594bce2eac079cfd
Nokokiii <kubanokoki@gmail.com>
2024-03-01 06:34:31 +0100

    async db

--- src/db.py
+++ src/db.py
@@ -4,33 +4,33 @@ from src.db_driver import connection as conn
 
 
 class Database:    
-    def get_users(self):
+    async def get_users(self):
         query = "SELECT * FROM Users;"
-        return asyncio.run(conn(query))
+        return await conn(query)
 
-    def get_user(self, user_id: str):
-        query = f'SELECT * FROM Users WHERE id = "{user_id}";'
-        return asyncio.run(conn(query))
+    async def get_user(self, user_id: str):
+        query = f'SELECT * FROM Users:{user_id};'
+        return await conn(query)
 
-    def create_user(self, data: dict):
+    async def create_user(self, data: dict):
         query = (f'CREATE Users SET firstName = "{data["firstName"]}", lastName = "{data["lastName"]}", birthYear = {data["birthYear"]}, group = "{data["group"]}";')
-        return asyncio.run(conn(query))
+        return await conn(query)
     
-    def update_user(self, user_id: str, is_firstname: str, is_lastname: str, is_birthyear: str, is_group: str, data: dict):
+    async def update_user(self, user_id: str, is_firstname: str, is_lastname: str, is_birthyear: str, is_group: str, data: dict):
         query = f'UPDATE Users:{user_id} SET '
 
         if is_firstname:
-            query += f'firstName = "{data["firstName"]}"'
+            query += f'firstName = "{data["firstName"]}",'
         if is_lastname:
-            query += f'lastName = "{data["lastName"]}"'
+            query += f'lastName = "{data["lastName"]}",'
         if is_birthyear:
-            query += f'birthYear = "{data["birthYear"]}"'
+            query += f'birthYear = {data["birthYear"]},'
         if is_group:
-            query += f'group = "{data["group"]}"'
+            query += f'group = "{data["group"]}",'
 
         query = f"{query[:-1]} WHERE id = Users:{user_id};"
-        return asyncio.run(conn(query))
+        return await conn(query)
     
-    def delete_user(self, user_id: str):
+    async def delete_user(self, user_id: str):
         query = f'DELETE FROM Users WHERE id = "{user_id}";'
-        return asyncio.run(conn(query)) 
+        return await conn(query)
