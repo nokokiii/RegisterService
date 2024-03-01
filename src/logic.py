a05d84702ab2ead3572a96b88e3086bce9fe752c
Nokokiii <kubanokoki@gmail.com>
2024-03-01 06:34:21 +0100

    update logic layer

--- src/logic.py
+++ src/logic.py
@@ -2,10 +2,11 @@ from src.db import Database
 
 
 class Logic:
-    def __init__(self):
+    def __init__(self, logger):
         self.db = Database()
+        self.logger = logger
 
-    def users_list_controller(self) -> dict:
+    async def users_list_controller(self) -> dict:
         """
         Return the list of users from the database.
 
@@ -18,17 +19,25 @@ class Logic:
             Exception: If there is a problem while getting users from the database.
         """
         try:
-            if db_res := self.db.get_users()[0] and db_res and "status" not in db_res:
-                if db_res["status"] == "OK":
-                    return {"status": "OK", "response": db_res["response"]}
-                elif db_res["status"] == "ERR":
-                    return {"status": "ERR", "response": db_res["response"], "error": "There was a problem while getting users"}
+            db_res = await self.db.get_users()
         except Exception as e:
+            self.logger.error("There was problem while getting data from database: ", str(e))
+            return {"status": "ERR", "response": "There was a problem while getting users", "error": str(e)}        
+        try:
+            if db_res and "status" in db_res:
+                if db_res["status"] == "OK":
+                    return {"status": "OK", "response": db_res["result"]}
+                elif db_res["status"] == "ERR":
+                    self.logger.error("There was a problem while getting users (prob db side problem): ", db_res)
+                    return {"status": "ERR", "response": db_res, "error": "There was a problem while getting users"}
+            self.logger.error("There was a problem while getting users (missing status in db_res): ", db_res)
+            return {"status": "ERR", "response": db_res, "error": "There was a problem while getting users"}
+        except Exception as e:
+            self.logger.error("There was problem while parasing db response: ", str(e))
             return {"status": "ERR", "response": "There was a problem while getting users", "error": str(e)}
-        return {"status": "ERR", "response": db_res, "error": "There was a problem while getting users"}
               
 
-    def user_controller(self, user_id: str) -> dict:
+    async def user_controller(self, user_id: str) -> dict:
         """
         Return the user information by ID.
 
@@ -44,19 +53,28 @@ class Logic:
             Exception: If there is a problem while getting the user from the database.
         """
         try:
-            if db_res := self.db.get_user(user_id)[0] and db_res and "status" not in db_res:
-                if db_res["status"] == "OK":
-                    return {"status": "OK", "response": db_res["response"]}
-                elif db_res["status"] == "ERR":
-                    return {"status": "ERR", "response": db_res["response"], "error": "There was a problem while getting the user"}
-                elif len(db_res["results"]) == 0:
-                    return {"status": "No_DATA", "response": "The user with provided id does not exist"}
+            db_res = await self.db.get_user(user_id)
         except Exception as e:
+            self.logger.error("There was problem while getting data from database: ", str(e))
             return {"status": "ERR", "response": "There was a problem while getting the user", "error": str(e)}
-        return {"status": "ERR", "response": db_res, "error": "There was a problem while getting the user"}
+        
+        try:
+            if db_res and "status" in db_res:
+                if "result" in db_res and len(db_res["result"]) > 0 and db_res["status"] == "OK":
+                    return {"status": "OK", "response": db_res["result"]}
+                elif "result" in db_res and db_res["status"] != "ERR" and len(db_res["result"]) == 0:
+                    return {"status": "No_DATA", "response": "The user with provided id does not exist"}
+                elif db_res["status"] == "ERR":
+                    self.logger.error("There was a problem while getting the user (prob db side problem): ", db_res)
+                    return {"status": "ERR", "response": db_res, "error": "There was a problem while getting the user"}
+            self.logger.error("There was a problem while getting the user (missing status in db_res): ", db_res)
+            return {"status": "ERR", "response": db_res, "error": "There was a problem while getting the user"}
+        except Exception as e:
+            self.logger.error("There was problem while parasing db response: ", str(e))
+            return {"status": "ERR", "response": "There was a problem while getting the user", "error": str(e)}
 
 
-    def create_user_controller(self, data: dict) -> dict:
+    async def create_user_controller(self, data: dict) -> dict:
         """
         Create user in the database.
 
@@ -72,21 +90,31 @@ class Logic:
             Exception: If there is a problem while creating the user in the database.
         """
         required_keys = ['firstName', 'lastName', 'birthYear', 'group']
-        if not all(key in data for key in required_keys):
+        if any(key not in data for key in required_keys):
             return {"status": "BAD_REQUEST", "response": "The provided data is missing values"}
 
         try:
-            if db_res := self.db.create_user(data)[0] and db_res and "status" not in db_res:
+            db_res = await self.db.create_user(data)
+        except Exception as e:
+            self.logger.error("There was problem while getting data from database: ", str(e))
+            return {"status": "ERR", "response": "There was a problem while creating the user", "error": str(e)}
+        
+        try:
+            if db_res and "status" in db_res:
                 if db_res["status"] == "OK":
                     return {"status": "OK", "response": "User created"}
                 elif db_res["status"] == "ERR":
-                    return {"status": "ERR", "response": db_res["response"], "error": "There was a problem while creating the user"}
+                    self.logger.error("There was a problem while creating the user (prob db side problem): ", db_res)
+                    return {"status": "ERR", "response": db_res, "error": "There was a problem while creating the user"}
+            self.logger.error("There was a problem while creating the user (missing status in db_res): ", db_res)
+            return {"status": "ERR", "response": db_res, "error": "There was a problem while creating the user"}
         except Exception as e:
+            self.logger.error("There was problem while parasing db response: ", str(e))
             return {"status": "ERR", "response": "There was a problem while creating the user", "error": str(e)}
-        return {"status": "ERR", "response": db_res, "error": "There was a problem while creating the user"}
 
 
-    def update_user_controller(self, user_id: str, data: dict) -> dict:
+
+    async def update_user_controller(self, user_id: str, data: dict) -> dict:
         """
         Update user in the database.
 
@@ -102,29 +130,36 @@ class Logic:
         Raises:
             Exception: If there is a problem while updating the user in the database.
         """
-        query = f"UPDATE Users:{user_id} SET "
-
-        is_firstname = True if "firstName" in data else "False"
-        is_lastname = True if "lastName" in data else "False"
-        is_birthyear = True if "birthYear" in data else "False"
-        is_group = True if "group" in data else "False"
+        is_firstname = "firstName" in data
+        is_lastname = "lastName" in data
+        is_birthyear = "birthYear" in data
+        is_group = "group" in data
 
 
         if not any([is_firstname, is_lastname, is_birthyear, is_group]):
-            return {"status": "BAD_REQUEST", "response": "The provided data is missing values", "error": "The provided data is missing values"}
+            return {"status": "BAD_REQUEST", "response": "The provided data is missing values"}
 
         try:
-            if db_res := self.db.update_user(query, is_firstname, is_lastname, is_birthyear, is_group)[0] and db_res and "status" not in db_res:
+            db_res = await self.db.update_user(user_id, is_firstname, is_lastname, is_birthyear, is_group, data)
+        except Exception as e:
+            self.logger.error("There was problem while getting data from database: ", str(e))
+            return {"status": "ERR", "response": "There was a problem while updating the user", "error": str(e)}
+
+        try:
+            if db_res and "status" in db_res:
                 if db_res["status"] == "OK":
                     return {"status": "OK", "response": "User updated"}
                 elif db_res["status"] == "ERR":
-                    return {"status": "ERR", "response": db_res["response"], "error": "There was a problem while updating the user"}
+                    self.logger.error("There was a problem while updating the user (prob db side problem): ", db_res)
+                    return {"status": "ERR", "response": db_res, "error": "There was a problem while updating the user"}
+            self.logger.error("There was a problem while updating the user (missing status in db_res): ", db_res)
+            return {"status": "ERR", "response": db_res, "error": "There was a problem while updating the user"}
         except Exception as e:
+            self.logger.error("There was problem while parasing db response: ", str(e))
             return {"status": "ERR", "response": "There was a problem while updating the user", "error": str(e)}
-        return {"status": "ERR", "response": db_res, "error": "There was a problem while updating the user"}
 
 
-    def delete_user_controller(self, user_id: str) -> dict:
+    async def delete_user_controller(self, user_id: str) -> dict:
         """
         Delete user from the database.
 
@@ -140,12 +175,20 @@ class Logic:
             Exception: If there is a problem while deleting the user from the database.
         """
         try:
-            if db_res := self.db.delete_user(user_id)[0] and db_res and "status" not in db_res:
+            db_res = await self.db.delete_user(user_id)
+        except Exception as e:
+            self.logger.error("There was problem while getting data from database: ", str(e))
+            return {"status": "ERR", "response": "There was a problem while deleting the user", "error": str(e)}
+        try:
+            if db_res and "status" in db_res:
                 if db_res["status"] == "OK":
                     return {"status": "OK", "response": "User deleted"}
                 elif db_res["status"] == "ERR":
-                    return {"status": "ERR", "response": db_res["response"], "error": "There was a problem while deleting the user"}
+                    self.logger.error("There was a problem while deleting the user (prob db side problem): ", db_res)
+                    return {"status": "ERR", "response": db_res, "error": "There was a problem while deleting the user"}
+            self.logger.error("There was a problem while deleting the user (missing status in db_res): ", db_res)
+            return {"status": "ERR", "response": db_res, "error": "There was a problem while deleting the user"}
         except Exception as e:
-            return {"status": "ERR", "response": "There was a problem while deleting the user", "error": str(e)}
-        return {"status": "ERR", "response": db_res, "error": "There was a problem while deleting the user"}
+            self.logger.error("There was problem while parasing db response: ", str(e))
+            return {"status": "ERR", "response": db_res, "error": "There was a problem while deleting the user"}
     
