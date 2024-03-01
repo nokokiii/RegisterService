5f3a234b1c843f2d48f606a1caf42060b100d7bb
Nokokiii <kubanokoki@gmail.com>
2024-03-01 06:34:42 +0100

    transition to quart

--- src/app.py
+++ src/app.py
@@ -1,110 +1,121 @@
 import asyncio
-import logging as log
 
-from flask import Flask, jsonify, request
+from quart import Quart, jsonify, request
 
 import src.status_code as sc
 from src.logic import Logic
 from src.db_driver import init_db
 
 
-app = Flask(__name__)
-logic = Logic()
+app = Quart(__name__)
+logic = Logic(app.logger)
 asyncio.run(init_db())
 
 
+@app.before_serving
+async def startup():
+    await init_db()
+
+
 @app.errorhandler(404)
-def page_not_found():
+async def page_not_found(e):
+    app.loggin.error(e)
     return jsonify({'message': 'Endpoint not found'}), sc.NOT_FOUND
 
 
 @app.get("/users")
-def get_users():
-    response = logic.users_list_controller()
+async def get_users():
+    """
+    Handle the GET request to get the list of users.
+    """
+    response = await logic.users_list_controller()
 
     if "status" not in response:
-        log.error(response)
+        app.logger.error(response)
         return jsonify({"message": "Internal Server Error"}), sc.INTERNAL_SERVER_ERROR
 
     if response["status"] == "OK":
-        return jsonify(response), sc.OK
+        return jsonify(response["response"]), sc.OK
     elif response["status"] == "ERR":
-        log.error(response["response"], response["error"])
         return jsonify({"message": "Internal Server Error"}), sc.INTERNAL_SERVER_ERROR
-    log.error(response)
+    app.logger.error(response)
     return jsonify({"message": "Internal Server Error"}), sc.INTERNAL_SERVER_ERROR
 
 
 @app.get("/users/<user_id>")
-def get_user(user_id: str):
-    response = logic.user_controller(user_id)
+async def get_user(user_id: str):
+    """
+    Handle the GET request to get the user by ID.
+    """
+    response = await logic.user_controller(user_id)
 
     if "status" not in response:
-        log.error(response)
+        app.logger.error(response)
         return jsonify({"message": "Internal Server Error"}), sc.INTERNAL_SERVER_ERROR
     
     if response["status"] == "OK":
         return jsonify(response), sc.OK
     elif response["status"] == "ERR":
-        log.error(response["response"], response["error"])
         return jsonify({"message": "Internal Server Error"}), sc.INTERNAL_SERVER_ERROR
     elif response["status"] == "No_DATA":
         return jsonify(response), sc.NOT_FOUND
-    log.error(response)
+    app.logger.error(response)
     return jsonify({"message": "Internal Server Error"}), sc.INTERNAL_SERVER_ERROR
 
 
 @app.post("/users")
-def create_user():
+async def create_user():
     data = request.get_json()
-    response = logic.create_user_controller(data)
+    response = await logic.create_user_controller(data)
 
     if "status" not in response:
-        log.error(response)
+        app.logger.error(response)
         return jsonify({"message": "Internal Server Error"}), sc.INTERNAL_SERVER_ERROR
+    
     
     if response["status"] == "OK":
         return jsonify(response), sc.CREATED
+    elif response["status"] == "BAD_REQUEST":
+        return jsonify(response), sc.BAD_REQUEST
     elif response["status"] == "ERR":
-        log.error(response["response"], response["error"])
         return jsonify({"message": "Internal Server Error"}), sc.INTERNAL_SERVER_ERROR
-    log.error(response)
+    app.logger.error(response)
     return jsonify({"message": "Internal Server Error"}), sc.INTERNAL_SERVER_ERROR
 
 
 
 @app.patch("/users/<user_id>")
-def update_user(user_id: str):
+async def update_user(user_id: str):
     data = request.get_json()
-    response = logic.update_user_controller(user_id, data)
+    response = await logic.update_user_controller(user_id, data)
 
     if "status" not in response:
-        log.error(response)
+        app.logger.error(response)
         return jsonify({"message": "Internal Server Error"}), sc.INTERNAL_SERVER_ERROR
     
     if response["status"] == "OK":
         return jsonify(response), sc.ACCEPTED
+    elif response["status"] == "BAD_REQUEST":
+        return jsonify(response), sc.BAD_REQUEST
     elif response["status"] == "ERR":
-        log.error(response["response"], response["error"])
         return jsonify({"message": "Internal Server Error"}), sc.INTERNAL_SERVER_ERROR
-    log.error(response)
+    app.logger.error(response)
     return jsonify({"message": "Internal Server Error"}), sc.INTERNAL_SERVER_ERROR
 
 
 @app.delete("/users/<user_id>")
-def delete_user(user_id: str):
-    response = logic.delete_user_controller(user_id)
+async def delete_user(user_id: str):
+    response = await logic.delete_user_controller(user_id)
 
     if "status" not in response:
-        log.error(response)
+        app.logger.error(response)
         return jsonify({"message": "Internal Server Error"}), sc.INTERNAL_SERVER_ERROR
 
     if response["status"] == "OK":
         return jsonify({"message": "User Deleted"}), sc.OK
     elif response["status"] == "ERR":
-        log.error(response["response"], response["error"])
         return jsonify({"message": "Internal Server Error"}), sc.INTERNAL_SERVER_ERROR
-    log.error(response)
+    app.logger.error(response)
     return jsonify({"message": "Internal Server Error"}), sc.INTERNAL_SERVER_ERROR
 
 
